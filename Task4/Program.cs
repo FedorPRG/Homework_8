91056fbe3fc1d1c6d64cc3e47449c45828a70c2e
Fedor <fedor_prg@rambler.ru>
2022-05-19 20:23:54 +1000

    Solving Task4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -6,10 +6,10 @@
         {
             for (int k = 0; k < matr.GetLength(2); k++)
             {
-                Console.Write($"{matr[i, j, k]} - {i}, {j}, {k}");
+                Console.Write($"{matr[i, j, k]} - {i}, {j}, {k}.");
                 Console.WriteLine();
-            }            
-        }        
+            }
+        }
     }
 }
 
@@ -19,14 +19,27 @@ void FillArray(int[,,] matr)
     {
         for (int j = 0; j < matr.GetLength(1); j++)
         {
-            for (int k = 0; k < matr.GetLength(2); k++)
+            matr[i, j, 0] = new Random().Next(10, 100);            
+            for (int k = 1; k < matr.GetLength(2); k++)
             {
-                matr[i, j, k] = new Random().Next(1, 10);
-            } 
+                matr[i, j, k] = new Random().Next(10, 100);
+                for (int l = 0; l < matr.GetLength(0); l++)
+                {
+                    for (int m = 0; m < matr.GetLength(1); m++)
+                    {                       
+                        for (int n = 0; n < k; n++)
+                        {
+                            if (matr[i, j, k] == matr[l, m, n])
+                            {
+                                k--; 
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
-
 Console.Write("Введите первый размер массива: ");
 int m = int.Parse(Console.ReadLine()!);
 
@@ -41,4 +54,3 @@ int[,,] matrix = new int[m, n, l];
 FillArray(matrix);
 PrintArray(matrix);
 Console.WriteLine();
-
--- Task5/Program.cs
+++ Task5/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void PrintArray(int[,,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            for (int k = 0; k < matr.GetLength(2); k++)
+            {
+                Console.Write($"{matr[i, j, k]} - {i}, {j}, {k}.");
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+void FillArray(int[,,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j, 0] = new Random().Next(10, 100);            
+            for (int k = 1; k < matr.GetLength(2); k++)
+            {
+                matr[i, j, k] = new Random().Next(10, 100);
+                for (int l = 0; l < matr.GetLength(0); l++)
+                {
+                    for (int m = 0; m < matr.GetLength(1); m++)
+                    {                       
+                        for (int n = 0; n < k; n++)
+                        {
+                            if (matr[i, j, k] == matr[l, m, n])
+                            {
+                                k--; 
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+Console.Write("Введите первый размер массива: ");
+int m = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второй размер массива: ");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите третий размер в массива: ");
+int l = int.Parse(Console.ReadLine()!);
+
+int[,,] matrix = new int[m, n, l];
+
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+
