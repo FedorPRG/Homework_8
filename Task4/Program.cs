c1e0caaa079e47ef7b83e9fdb6eeadfb18f66a0c
Fedor <fedor_prg@rambler.ru>
2022-05-20 21:38:15 +1000

    Updated Task4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -19,19 +19,19 @@ void FillArray(int[,,] matr)
     {
         for (int j = 0; j < matr.GetLength(1); j++)
         {
-            matr[i, j, 0] = new Random().Next(10, 100);            
+            matr[i, j, 0] = new Random().Next(10, 100);
             for (int k = 1; k < matr.GetLength(2); k++)
             {
                 matr[i, j, k] = new Random().Next(10, 100);
                 for (int l = 0; l < matr.GetLength(0); l++)
                 {
                     for (int m = 0; m < matr.GetLength(1); m++)
-                    {                       
+                    {
                         for (int n = 0; n < k; n++)
                         {
                             if (matr[i, j, k] == matr[l, m, n])
                             {
-                                k--; 
+                                k--;
                             }
                         }
                     }
@@ -48,6 +48,13 @@ int n = int.Parse(Console.ReadLine()!);
 
 Console.Write("Введите третий размер в массива: ");
 int l = int.Parse(Console.ReadLine()!);
+
+if (m * n * l > 89)
+{
+    Console.WriteLine($"Количество эелементов заданной матрицы {m}*{n}*{l}={n * m * l} больше" +
+     " количества неповторяющихся чисел от 10 до 99");
+    return;
+}
 
 int[,,] matrix = new int[m, n, l];
 
